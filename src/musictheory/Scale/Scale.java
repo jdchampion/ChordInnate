d540785fe1eb4076f0acda017e427fb888f89ffd
jdchampion <Joseph@Josephs-MBP.westell.com>
2015-12-31 12:23:19 -0600

    a bit more clarification

--- src/musictheory/Scale/Scale.java
+++ src/musictheory/Scale/Scale.java
@@ -11,9 +11,9 @@ public class Scale {
     private static String rootName;
     private static int root;
     private static ScaleType scaleType;
-    private ArrayList<Note> notes = new ArrayList<>();
+    private ArrayList<Note> scaleDegrees = new ArrayList<>();
     private static int octaveRange;
-    private static int currentOctave;
+    private static int currentOctave = 0; // Default set to 0
 
     protected Scale(String rootName, int root, ScaleType scaleType) {
         this.rootName = rootName;
@@ -22,15 +22,20 @@ public class Scale {
 
         int[] sequence = scaleType.getSequence();
         for (int i = 0; i < scaleType.getNumberOfNotes(); i++) {
-            notes.add(i, NoteFactory.buildNote(getScaleDegreeName(sequence[i])));
+
+            // Each note is considered a degree of the scale.
+            scaleDegrees.add(i, NoteFactory.buildNote(getScaleDegreeName(sequence[i])));
         }
 
-//        this.octaveRange = notes.get(0).
+        // The octave range of the scale == the octave range of the root note.
+        this.octaveRange = scaleDegrees.get(0).getOctaveRange();
     }
 
     protected int getOctaveRange() {
         return octaveRange;
     }
+
+    protected int getCurrentOctave() { return currentOctave; }
 
     protected String getRootName() {
         return rootName;
