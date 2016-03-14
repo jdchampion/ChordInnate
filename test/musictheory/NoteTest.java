3b71558e1e5f86020814771e32f4670a4b203fc6
jdchampion <Joseph@Josephs-MBP.westell.com>
2016-03-13 20:08:57 -0500

    Tests for Note and Scale

--- test/musictheory/NoteTest.java
+++ test/musictheory/NoteTest.java
@@ -1,61 +1,99 @@
 package musictheory;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
 /**
  * Created by Joseph on 3/13/16.
  */
+
+@RunWith(Parameterized.class)
 public class NoteTest {
 
-    @Test
-    public void testSetOctave() throws Exception {
+    private Note note;
+    private NoteType noteType;
+    private int octave;
 
+    public NoteTest(NoteType noteType, int octave) {
+        this.noteType = noteType;
+        this.octave = octave;
+        this.note = new Note(noteType);
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        List<Object[]> data = new ArrayList<Object[]>();
+        for (NoteType nt : NoteType.values()) {
+            for (int i = 0; i < 12; i++) {
+                data.add(new Object[] {nt, i});
+            }
+        }
+
+        return data;
     }
 
     @Test
-    public void testGetNoteType() throws Exception {
-
-    }
-
-    @Test
-    public void testGetOctave() throws Exception {
-
-    }
-
-    @Test
-    public void testGetName() throws Exception {
-
+    public void testOctave() throws Exception {
+        note.setOctave(octave);
+        assertFalse(note.getOctave() < 0);
     }
 
     @Test
     public void testGetRelativePitch() throws Exception {
-
+        int relativePitch = note.getRelativePitch();
+        assertTrue(relativePitch >= 0 && relativePitch < 128);
     }
 
     @Test
     public void testGetLetter() throws Exception {
-
+        char letter = note.getLetter();
+        assertTrue(letter >= 'A' && letter <= 'G');
     }
 
     @Test
     public void testGetAccidental() throws Exception {
-
+        Accidental accidental = note.getAccidental();
+        assertNotNull(accidental);
     }
 
     @Test
     public void testHasAccidentalSymbol() throws Exception {
-
+        Accidental accidental = note.getAccidental();
+        if (accidental.equals(Accidental.NONE)) {
+            assertFalse(note.hasAccidentalSymbol());
+        }
+        else {
+            assertTrue(note.hasAccidentalSymbol());
+        }
     }
 
     @Test
     public void testIsNatural() throws Exception {
-
+        Accidental accidental = note.getAccidental();
+        if (accidental.equals(Accidental.NATURAL)) {
+            assertTrue(note.isNatural());
+        }
+        else {
+            assertFalse(note.isNatural());
+        }
     }
 
     @Test
     public void testIsDoubleAccidental() throws Exception {
-
+        Accidental accidental = note.getAccidental();
+        if (accidental.equals(Accidental.DOUBLE_FLAT)
+                || accidental.equals(Accidental.DOUBLE_SHARP)) {
+            assertTrue(note.isDoubleAccidental());
+        }
+        else {
+            assertFalse(note.isDoubleAccidental());
+        }
     }
 }
--- test/musictheory/ScaleTest.java
+++ test/musictheory/ScaleTest.java
@@ -1,62 +1,106 @@
 package musictheory;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.*;
 
 import static org.junit.Assert.*;
 
 /**
  * Created by Joseph on 3/13/16.
  */
+
+@RunWith(Parameterized.class)
 public class ScaleTest {
+
+    private Scale scale;
+
+    public ScaleTest(Scale scale) {
+        this.scale = scale;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Scale> data() {
+        List<Scale> data = new ArrayList<Scale>();
+        for (ScaleType st : ScaleType.values()) {
+            for (NoteType nt : NoteType.values()) {
+                data.add(new Scale(nt, st));
+            }
+        }
+
+        return data;
+    }
 
     @Test
     public void testSetNoteOctaves() throws Exception {
+        scale.setNoteOctaves(5);
+        Note[] notes = scale.getAscendingNotes();
 
+        // Ascending notes should have increasing relative pitch
+        for (int i = 1; i < notes.length; i++) {
+            assertTrue(notes[i-1].getRelativePitch() < notes[i].getRelativePitch());
+        }
     }
 
     @Test
     public void testSetDiatonicChordTypes() throws Exception {
+        Set diatonicChordTypes = scale.getDiatonicChordTypes();
+        assertNotNull(diatonicChordTypes);
+        assertFalse(diatonicChordTypes.isEmpty());
 
     }
 
     @Test
     public void testGetScaleType() throws Exception {
-
+        assertNotNull(scale.getScaleType());
     }
 
     @Test
     public void testGetKeySignature() throws Exception {
-
+        assertNotNull(scale.getKeySignature());
     }
 
     @Test
     public void testGetSteps() throws Exception {
-
+        Step[] steps = scale.getSteps();
+        assertNotNull(steps);
+        assertEquals(scale.getNotes().length - 1, steps.length);
     }
 
     @Test
     public void testGetAscendingNotes() throws Exception {
-
+        Note[] ascendingNotes = scale.getAscendingNotes();
+        assertNotNull(ascendingNotes);
+        assertEquals(scale.getNotes().length, ascendingNotes.length);
     }
 
     @Test
     public void testGetDescendingNotes() throws Exception {
-
+        Note[] descendingNotes = scale.getDescendingNotes();
+        assertNotNull(descendingNotes);
+        assertEquals(scale.getNotes().length, descendingNotes.length);
     }
 
     @Test
     public void testGetNashvilleNumbers() throws Exception {
-
+        NashvilleNumber[] nashvilleNumbers = scale.getNashvilleNumbers();
+        assertNotNull(nashvilleNumbers);
     }
 
     @Test
     public void testGetDiatonicChordTypes() throws Exception {
-
+        Set diatonicChordTypes = scale.getDiatonicChordTypes();
+        assertNotNull(diatonicChordTypes);
     }
 
     @Test
     public void testGetDiatonicChordTypesByRelativePitch() throws Exception {
-
+        HashMap<Integer, ArrayList<ChordType>> diatonicChordTypesByRelativePitch = scale.getDiatonicChordTypesByRelativePitch();
+        assertNotNull(diatonicChordTypesByRelativePitch);
+        Set diatonicChordTypes = scale.getDiatonicChordTypes();
+        assertTrue(diatonicChordTypes.containsAll(diatonicChordTypes));
     }
 
     @Test
