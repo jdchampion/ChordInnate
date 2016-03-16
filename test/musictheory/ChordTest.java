52d537064e8adc014ba7fa5a64810948278ebb9a
jdchampion <Joseph@Josephs-MBP.westell.com>
2016-03-15 21:02:09 -0500

    Added playback functionality for ChordTest

--- test/musictheory/ChordTest.java
+++ test/musictheory/ChordTest.java
@@ -1,9 +1,15 @@
 package musictheory;
 
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
+import javax.sound.midi.MidiChannel;
+import javax.sound.midi.MidiSystem;
+import javax.sound.midi.MidiUnavailableException;
+import javax.sound.midi.Synthesizer;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -17,10 +23,40 @@ import static org.junit.Assert.*;
 @RunWith(Parameterized.class)
 public class ChordTest {
 
+    private final boolean PLAYBACK = false;
+    private final int PLAYBACK_VOLUME = 127;
+    private final int PLAYBACK_DURATION = 120;
+    private final int PLAYBACK_WAIT = 100;
+
     private Chord chord;
+
+    static MidiChannel[] channels;
+    static Synthesizer synthesizer;
 
     public ChordTest(Chord chord) {
         this.chord = chord;
+    }
+
+    @BeforeClass
+    public static void setUp() {
+        try {
+            synthesizer = MidiSystem.getSynthesizer();
+            synthesizer.open();
+            channels = synthesizer.getChannels();
+
+            Thread.sleep(1000); // To minimize the (annoying) initial sound delay
+        }
+        catch (MidiUnavailableException ex) {}
+        catch (InterruptedException ex) {}
+        catch (Exception ex) {}
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        try {
+            synthesizer.close();
+        }
+        catch (Exception ex) {}
     }
 
     @Parameterized.Parameters
@@ -44,6 +80,10 @@ public class ChordTest {
         for (int i = chord.minOctave; i < chord.maxOctave; i++) {
             c1.setNoteOctaves(i);
             c2.setOctave(i);
+
+            testSoundChord(c1);
+            testSoundChord(c2);
+
             for (int j = 0; j < notes.length; j++) {
                 oct1 = n1[j].getOctave();
                 oct2 = n2[j].getOctave();
@@ -64,6 +104,14 @@ public class ChordTest {
             for (int j = 0; j < i; j++) {
                 c.invert();
             }
+
+            System.out.print(c.name + " : ");
+            for (Note m : c.notes) {
+                System.out.print(m.getName() + m.getOctave() + " ");
+            }
+            System.out.println();
+
+            testSoundChord(c);
 
             // Ensure that the correct Notes were raised by one octave
             Note[] inverted = c.getNotes();
@@ -94,5 +142,27 @@ public class ChordTest {
         int inversionNumber = chord.getInversionNumber();
         assertNotNull(inversionNumber);
         assertFalse(inversionNumber < 0);
+    }
+
+    private void testSoundChord(Chord chord) {
+        if (chord != null) {
+            soundChord(chord, PLAYBACK_VOLUME, PLAYBACK_DURATION, PLAYBACK_WAIT);
+        }
+    }
+
+    private void soundChord(Chord chord, int volume, int duration, int wait) {
+        if (PLAYBACK)
+            try {
+                Note[] notes = chord.notes;
+                for (Note n : notes) {
+                    channels[0].noteOn(n.getRelativePitch(), volume);
+                }
+                Thread.sleep(duration);
+                for (Note n : notes) {
+                    channels[0].noteOff(n.getRelativePitch(), volume);
+                }
+                Thread.sleep(wait);
+            }
+            catch (InterruptedException ex) {}
     }
 }
