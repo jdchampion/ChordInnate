e1df27a0e15db666639e461ca290dbf5e147f0c7
jdchampion <Joseph@Josephs-MacBook-Pro.local>
2016-03-18 20:41:26 -0500

    Added outside package test

---
+++ test/PackageTest.java
@@ -0,0 +1,52 @@
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import javax.sound.midi.MidiChannel;
+import javax.sound.midi.MidiSystem;
+import javax.sound.midi.MidiUnavailableException;
+import javax.sound.midi.Synthesizer;
+import java.util.*;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Joseph on 3/18/16.
+ */
+
+public class PackageTest {
+
+    static MidiChannel[] channels;
+    static Synthesizer synthesizer;
+
+    public PackageTest() {}
+
+    @BeforeClass
+    public static void setUp() {
+        try {
+            synthesizer = MidiSystem.getSynthesizer();
+            synthesizer.open();
+            channels = synthesizer.getChannels();
+
+            Thread.sleep(1000); // To minimize the (annoying) initial sound delay
+        }
+        catch (MidiUnavailableException ex) {}
+        catch (InterruptedException ex) {}
+        catch (Exception ex) {}
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        try {
+            synthesizer.close();
+        }
+        catch (Exception ex) {}
+    }
+
+    @Test
+    public void testPackage() throws Exception {
+
+    }
+}
