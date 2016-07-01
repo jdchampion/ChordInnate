d399f9a03d19ee9bf4184c5e1806b17c8f1c8258
jdchampion <bandgeek47@gmail.com>
2016-07-01 16:03:22 -0500

    checkCompoundSums() works

--- test/chordinnate/musictheory/TestBeat.java
+++ test/chordinnate/musictheory/TestBeat.java
@@ -39,23 +39,92 @@ public class TestBeat {
 
     @Test
     public void checkCompoundSums() throws Exception {
-        assertEquals(
-                HALF.getRatio(),
-                DOTTED_QUARTER.getRatio() + EIGHTH.getRatio(),
-                0
-        );
+        Beat[] standardLengths = {SIXTY_FOURTH, THIRTY_SECOND, SIXTEENTH, EIGHTH, QUARTER, HALF, WHOLE, DOUBLE_WHOLE};
 
-        assertEquals(
-                HALF.getRatio(),
-                DOTTED_QUARTER.getRatio() + SIXTEENTH.getRatio() + SIXTEENTH.getRatio(),
-                0
-        );
+        /*
+         * Test all standard subdivisions.
+         * Each subdivision should == 2 * the next largest subdivision.
+         */
+        for (int i = 1; i < standardLengths.length; i++) {
+            for (int j = i - 1; j > 0; j--) {
+                assertEquals(
+                        standardLengths[i].getRatio(),
+                        (Math.pow(2, (i - j))) * standardLengths[j].getRatio(),
+                        0
+                );
+            }
+        }
 
-        assertEquals(
-                QUARTER.getRatio(),
-                TRIPLET_EIGHTH.getRatio() + TRIPLET_EIGHTH.getRatio() + TRIPLET_EIGHTH.getRatio(),
-                0.001
-        );
+        /*
+         * If all subdivisions above pass,
+         * then the combination of a
+         * dotted Beat and any shorter
+         * standard subdivisions should also pass.
+         *
+         * For example, all of these should equal a 1/2 note
+         * (D == dotted):
+         *
+         * D(1/4) + 1/8
+         * D(1/4) + D(1/8) + 1/16
+         * D(1/4) + (4 * 1/16)
+         * D(1/4) + (8 * 1/32)
+         * 1/4 + D(1/8) + 1/16
+         *
+         * ... and so on.
+         *
+         * These should pass with exact precision
+         * (delta == 0) in the assertion.
+         */
+        for (int i = 2; i < standardLengths.length; i++) {
+            assertEquals(
+                    standardLengths[i].getRatio(),
+                    Beat.valueOf("DOTTED_" + standardLengths[i - 1].name()).getRatio()
+                    + standardLengths[i - 2].getRatio(),
+                    0
+            );
+        }
+
+        /*
+         * Test all tuplet subdivisions.
+         * Since some of these will return
+         * a sum that is a repeating decimal
+         * (which continues beyond precision of java type "double"),
+         * the delta must accept a small amount of error
+         * (delta == 0.00000000001).
+         */
+        for (Duration duration : Duration.values()) {
+            if (duration.equals(Duration.SIXTY_FOURTH)) continue;
+            for (Tuplet tuplet : Tuplet.values()) {
+                if (tuplet.equals(Tuplet.NONE)) continue;
+                assertEquals(
+                        Beat.valueOf(duration + "").getRatio(),
+                        tupleSum(Beat.valueOf(tuplet + "_" + duration.getPrevious())),
+                        0.00000000001
+                );
+            }
+        }
+
+        /*
+         * Assuming all tests pass above,
+         * any combination of dots or tuplets
+         * will return the correct lengths.
+         *
+         * As noted previously, certain tuplets
+         * will return repeating decimal values
+         * as their sums, so be sure to account
+         * for the precision.
+         */
+    }
+
+    private double tupleSum(Beat beat) {
+        if (beat.getTuplet().equals(Tuplet.NONE) || !beat.getDotValue().equals(DotValue.NONE)) {
+            throw new IllegalArgumentException("tupleSum() can only handle pure Tuplets");
+        }
+        double sum = 0;
+        for (int i = 0; i < beat.getTuplet().getNumber(); i++) {
+            sum += beat.getRatio();
+        }
+        return sum;
     }
 
 }
