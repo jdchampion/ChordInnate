3fbb21f648d8c4659af1cd437fdcc5da7b5184e7
jdchampion <bandgeek47@gmail.com>
2016-07-03 11:45:45 -0500

    rational / irrational meter

---
+++ src/chordinnate/musictheory/time/meter/IrrationalMeter.java
@@ -0,0 +1,12 @@
+package chordinnate.musictheory.time.meter;
+
+/**
+ * Created by Joseph on 7/3/16.
+ */
+public class IrrationalMeter extends FixedMeter {
+    @Override
+    public boolean is(MeterProperty meterProperty) {
+        // TODO
+        return false;
+    }
+}
