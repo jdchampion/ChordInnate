1ece5ffe5045056e60885fcb7952093396f54fef
jdchampion <bandgeek47@gmail.com>
2016-07-06 20:14:58 -0500

    Fix tests

--- test/chordinnate/musictheory/time/meter/TestAdditiveCompleteMeter.java
+++ test/chordinnate/musictheory/time/meter/TestAdditiveCompleteMeter.java
@@ -3,6 +3,7 @@ package chordinnate.musictheory.time.meter;
 import chordinnate.musictheory.time.rhythm.Duration;
 import org.junit.Test;
 
+import static chordinnate.musictheory.time.meter.MeterSubdivision.*;
 import static org.junit.Assert.*;
 
 /**
@@ -12,14 +13,13 @@ public class TestAdditiveCompleteMeter {
     @Test
     public void is() throws Exception {
         AdditiveCompleteMeter
-                fourFour1 = new AdditiveCompleteMeter(4, Duration.QUARTER),
-                nineEight = new AdditiveCompleteMeter(9, Duration.EIGHTH),
-                sixEight = new AdditiveCompleteMeter(6, Duration.EIGHTH),
-                threeEight = new AdditiveCompleteMeter(3, Duration.EIGHTH),
-                twelveEight = new AdditiveCompleteMeter(12, Duration.EIGHTH),
-                fourFour2 = new AdditiveCompleteMeter(4, Duration.QUARTER, MeterSubdivision.QUADRUPLE),
-                oneFour = new AdditiveCompleteMeter(1, Duration.QUARTER),
-                elevenSixteen = new AdditiveCompleteMeter(11, Duration.SIXTEENTH);
+                fourFour1 = new AdditiveCompleteMeter(4, Duration.QUARTER, DUPLE, DUPLE),
+                nineEight = new AdditiveCompleteMeter(9, Duration.EIGHTH, DUPLE, TRIPLE, QUADRUPLE),
+                sixEight = new AdditiveCompleteMeter(6, Duration.EIGHTH, DUPLE, DUPLE, DUPLE),
+                threeEight = new AdditiveCompleteMeter(3, Duration.EIGHTH, TRIPLE),
+                twelveEight = new AdditiveCompleteMeter(12, Duration.EIGHTH, TRIPLE, QUADRUPLE, DUPLE, TRIPLE),
+                fourFour2 = new AdditiveCompleteMeter(4, Duration.QUARTER, QUADRUPLE),
+                elevenSixteen = new AdditiveCompleteMeter(11, Duration.SIXTEENTH, QUADRUPLE, QUADRUPLE, TRIPLE);
 
         System.out.print("6/8: ");
         for (MeterClassificationType mct : MeterClassificationType.values()) {
