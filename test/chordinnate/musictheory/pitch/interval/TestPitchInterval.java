86a6c5f679b85150efe7c93347935ab3bebd1e4a
jdchampion <bandgeek47@gmail.com>
2016-07-18 00:12:33 -0500

    Forgot these

--- test/chordinnate/musictheory/pitch/interval/TestPitchInterval.java
+++ test/chordinnate/musictheory/pitch/interval/TestPitchInterval.java
@@ -126,8 +126,8 @@ public class TestPitchInterval {
         assertEquals(vii, PitchInterval.MINOR_SEVENTH.ROMAN_NUMERAL);
         assertEquals(VI, PitchInterval.AUGMENTED_SIXTH.ROMAN_NUMERAL);
         assertEquals(VII, PitchInterval.MAJOR_SEVENTH.ROMAN_NUMERAL);
-        assertEquals(viii, PitchInterval.DIMINISHED_OCTAVE.ROMAN_NUMERAL);
-        assertEquals(VIII, PitchInterval.PERFECT_OCTAVE.ROMAN_NUMERAL);
+        assertEquals(i, PitchInterval.DIMINISHED_OCTAVE.ROMAN_NUMERAL);
+        assertEquals(I, PitchInterval.PERFECT_OCTAVE.ROMAN_NUMERAL);
         assertEquals(VII, PitchInterval.AUGMENTED_SEVENTH.ROMAN_NUMERAL);
     }
 
