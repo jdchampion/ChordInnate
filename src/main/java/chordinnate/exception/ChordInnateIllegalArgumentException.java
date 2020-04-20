96736d9b5758671d6a04ff130c82a7ecb75d26f3
jdchampion <jdchampion@users.noreply.github.com>
2020-04-20 09:29:39 -0500

    ChordInnateIllegalArgumentException

---
+++ src/main/java/chordinnate/exception/ChordInnateIllegalArgumentException.java
@@ -0,0 +1,21 @@
+package chordinnate.exception;
+
+public class ChordInnateIllegalArgumentException extends ChordInnateException {
+
+    public ChordInnateIllegalArgumentException() {
+        super();
+    }
+
+    public ChordInnateIllegalArgumentException(String s) {
+        super(s);
+    }
+
+    public ChordInnateIllegalArgumentException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ChordInnateIllegalArgumentException(Throwable cause) {
+        super(cause);
+    }
+
+}
